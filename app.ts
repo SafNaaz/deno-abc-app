e19928f99815bf1a9e4d74d7f914d4d4b77ff386
SAFNAS <iamsafnas@gmail.com>
2020-07-09 23:55:57 +0530

    added a route

--- app.ts
+++ app.ts
@@ -2,5 +2,10 @@ import { Application } from "https://deno.land/x/abc@v1/mod.ts";
 
 const app = new Application();
 
+//routes
+app.get("/", async () => {
+  console.log("request made");
+});
+
 //listen to port
 app.start({ port: 3000 });
