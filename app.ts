2bf2308d7843f7fb646ce40825ff3583fecb281d
SAFNAS <iamsafnas@gmail.com>
2020-07-10 00:48:43 +0530

    added model, controller and seperated routes

--- app.ts
+++ app.ts
@@ -1,4 +1,10 @@
 import { Application, Context } from "https://deno.land/x/abc@v1/mod.ts";
+import {
+  get_all_books,
+  get_book,
+  create_book,
+  delete_book,
+} from "./controllers/bookController.ts";
 
 const app = new Application();
 
@@ -11,12 +17,10 @@ app.get("/", async (ctx: Context) => {
 });
 
 app
-  .get("/books", (ctx: Context) => {
-    ctx.json([{ title: "blah" }, { title: "blah blah" }], 200);
-  })
-  .get("/books/:id", () => {})
-  .post("/books", () => {})
-  .delete("/books/:id", () => {});
+  .get("/books", get_all_books)
+  .get("/books/:id", get_book)
+  .post("/books", create_book)
+  .delete("/books/:id", delete_book);
 
 //listen to port
 app.start({ port: 3000 });
